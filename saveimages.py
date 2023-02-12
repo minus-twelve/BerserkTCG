4f704eb9d6a4ffe02def48660cd9f121166e91cb
minus-twelve <121825645+minus-twelve@users.noreply.github.com>
2023-02-12 23:40:30 +0300

    Update saveimages.py

--- saveimages.py
+++ saveimages.py
@@ -1,10 +1,11 @@
-import urllib.request
+from urllib.request import Request, urlopen
 
 i=1
 while i<31:
     a=i
     url = "https://www.laststicker.ru/i/cards/51/"+str(a)+".jpg"
-    r = urllib.request.urlopen(url)
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    r = urlopen(req)
     with open(f"{str(a)}.jpg", "wb") as f:
         f.write(r.read())
         f.close()
